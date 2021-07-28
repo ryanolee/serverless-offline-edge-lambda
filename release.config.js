58dc4eab99f0ba6728829cf713cb2c939e6b4145
ryan lee <rol@bluetel.co.uk>
2021-07-28 13:40:30 +0100

    Drop release packages

--- release.config.js
+++ release.config.js
@@ -2,9 +2,6 @@ module.exports = {
 	branch: 'master',
 	tagFormat: '${version}',
 	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/changelog',
 		'@semantic-release/npm',
 		'@semantic-release/github',
 		['@semantic-release/git', {
